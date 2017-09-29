62220af0408b9353830de1feda7fe362b8de2739
razum-force <razum.force@gmail.com>
2017-09-29 12:15:50 +0400

    homework 1 and 2 - done test.html file makes output with 'alert' (and not to console)

--- ror1.cs
+++ ror1.cs
@@ -1,8 +1,18 @@
+#ЗАДАНИЕ 1
 sum = (a, b) -> a + b
 
+#ЗАДАНИЕ 2
+weekday = (date) ->
+  WEEKDAYS = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+  day = date.getDay()
+  WEEKDAYS[day-1]
+
+#ТЕСТ ЗАДАНИЯ 1
 a = +prompt "enter number a"
 b = +prompt "enter number b"
+alert "Сумма будет = #{sum(a,b)}"
 
-console.log sum(a,b)
-
+#ТЕСТ ЗАДАНИЯ 2
+today = new Date
+alert "Сегодня: #{today}, а день недели #{weekday(today)}"
 
